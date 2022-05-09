92d637dc6beb9d6d08fb9a8e84819bffa34b4da4
Anderson <anderson.gomez@utp.edu.co>
2022-05-09 10:51:29 -0500

    colisiones

--- parcial 2/parcial2/juego.js
+++ parcial 2/parcial2/juego.js
@@ -20,7 +20,7 @@ loadSprite('mono1','5rvINjw.png')
 loadSprite('mono2','lVzMkL9.png')
 
 /**Carga de la escena de juego. */
-scene("juego", ()=>{
+scene("juego", ({score})=>{
     layers(['bg','obj','ui'],'obj')
     /**Creacion del mapa */
     const map = [
@@ -55,8 +55,45 @@ scene("juego", ()=>{
         RIGHT: "right"
     }
 
+    /**
+     * Definicion de la escena lose, 
+     * se muestran detalles de la puntuacion lograda
+     * asi como los eventos necesarios para
+     * empezar una nueva partida.
+     */
+    scene("lose", ({score})=>{
+        add([
+            text(`game over, \nyou score is: ${score}`),
+            scale(0.5),
+        ])
+        add([
+            text(`push enter for \nto play again.`),
+            pos(0,100),
+            scale(0.5),
+        ])
+        onKeyPress("enter",()=>{
+            go("juego", ({score:0}))
+        })
+    })
 
-    const mario = add([
+    /**
+     * variable que registra la puntuacion del juego en pantalla.
+     */
+    const puntuacion = add([
+        text(score),
+        pos(0,-120),
+        scale(0.5),
+        layer("ui",),
+        {
+            value: score,
+        }
+
+    ])
+
+    /**
+     * Definicion del personaje principal.
+     */
+    const charlie = add([
         sprite('charlie'), 
         pos(40,0),
         scale(0.6),
@@ -65,7 +102,9 @@ scene("juego", ()=>{
         body()
     ])
 
-
+    /**
+     * Definicion del primer enemigo.
+     */
     const mono =add ([
         sprite('mono1'),
         pos(460,60),
@@ -74,11 +113,15 @@ scene("juego", ()=>{
         body(),
         solid(),
         "mono",
+        "enemy",
         {
             vel : ENEMIE_SPEED
         }
     ])
 
+    /**
+     * Definicion del segundo enemigo.
+     */
     const mono2 =add ([
         sprite('mono1'),
         pos(200,60),
@@ -87,6 +130,7 @@ scene("juego", ()=>{
         body(),
         solid(),
         "salto",
+        "enemy",
         {
             vel : ENEMIE_SPEED
         }
@@ -101,6 +145,7 @@ scene("juego", ()=>{
             solid(),
             "move",
             "monoBlue",
+            "enemy",
             {
                 dir: true,
                 vel:ENEMIE_SPEED*-1
@@ -109,6 +154,10 @@ scene("juego", ()=>{
     }
     spawnMonoBlue();
 
+    /**
+     * Para cada frame el objeto con la etiqueta mono
+     * ejerce un movimiento.
+     */
     onUpdate("mono", (e) =>{
         e.move(e.vel,0);
         if(e.pos.x < 0){
@@ -126,13 +175,19 @@ scene("juego", ()=>{
         
     })
 
+    /**
+     * Para cada frame el objeto con la etiqueta salto
+     * ejerce un salto.
+     */
     onUpdate("salto", (e)=>{
         if(e.isGrounded()){
             e.jump(800)
         }
     })
 
-
+    /**Cada 0.5s el objeto que tiene la etiqueta monoBlue, ejerce
+     * un momvimiento de derecha a izquierda.
+     */
     loop(0.5,()=>{
         every("monoBlue", (t)=>{
             if(t.dir==true){
@@ -153,24 +208,59 @@ scene("juego", ()=>{
 
     })
 
+    /** Se realizan acciones al detectar presion sobre las teclas 
+     * right: movimiento hacia la derecha. 
+     * left: movimiento hacia la izquierda.
+     * space: salto.
+     * */
     onKeyDown("right", ()=>{
-        mario.use(sprite("charlie")) //cambiar el sprite.
-        mario.move(150,0)
+        charlie.use(sprite("charlie")) //cambiar el sprite.
+        charlie.move(150,0)
     })
 
     onKeyDown("left", ()=>{
-        mario.flipX(true)
-        mario.move(-150,0)
+        charlie.flipX(true)
+        charlie.move(-150,0)
     })
 
     onKeyPress("space",()=>{
-        if(mario.isGrounded())
-        mario.jump(400)
+        if(charlie.isGrounded())
+        charlie.jump(400)
     })
 
-    mario.onUpdate(()=>{
-        camPos(mario.pos)
+    /**La camara del juego sigue al personaje principal. */
+    charlie.onUpdate(()=>{
+        camPos(charlie.pos)
     })
+
+    /**
+     * Cuando el personaje principal, colicione con
+     * un objeto que tenga la etiqueta enemy, desencadena
+     * los evento de destruccion y cambio de escena.
+     */
+    charlie.onCollide("enemy", ()=>{
+        destroy(charlie);
+        
+        wait(0.5, ()=>{
+            
+            go("lose",({score:puntuacion.value}));
+        })
+    })
+
+    /**Cada segundo se llama a la funcion que actualiza
+     * la puntuacion del juego.
+     */
+    loop(1,()=>{
+        puntos();
+    })
+    
+    /**
+     * funcion que actualiza la puntuacion.
+     */
+    function puntos (){
+        puntuacion.value += 1;
+        puntuacion.text = puntuacion.value;
+    }
 
 })
-go("juego")
+go("juego", ({score:0}))
---
+++ practica_9/Ejemplo_1/juego.js
@@ -0,0 +1,254 @@
+kaboom({
+    width: 360,
+    height: 360,
+    global: true,
+    fullscreen: true,
+    scale: 1.0,
+    clearColor: [0,0,0,1],
+})
+
+
+
+/**
+ * Carga de Sprites.
+ */
+loadRoot('https://i.imgur.com/')
+loadSprite('bloque','pogC9x5.png')
+loadSprite('marioDer','CtLdqAd.png')
+loadSprite('marioIzq', 'Jn6wQoo.png')
+loadSprite('enemigo','C90Lmfl.png')
+loadSprite('enemigo2','kcF4pyj.png')
+loadSprite('tortuga1', 'TCvlZ3d.png')
+loadSprite('tortuga2', '9HBzqO1.png')
+
+/**Carga de la escena de juego. */
+scene("juego", ()=>{
+    layers(['bg','obj','ui'],'obj')
+    /**Creacion del mapa */
+    const map = [
+        '                               ',
+        '                               ',
+        '                               ',
+        '            =                  ',
+        '=                              ',
+        '                     =         ',
+        '                               ',
+        '========================   ====',
+    ]
+    /**definicion de los simbolos en el mapa*/
+    const levelCfg = {
+         width: 20,
+         height: 20,
+         '=': ()=>[sprite('bloque'), area(),solid()]
+     }
+    const gameLevel = addLevel(map, levelCfg)
+    
+
+    /**Constantes del juego */
+    ENEMIE_SPEED = 100;
+    const directions = {
+        LEFT : "left",
+        RIGHT: "right"
+    }
+    const enemie_animations=["enemigo", "enemigo2"];
+    const tortuga_animations=["tortuga1", "tortuga2"];
+
+    //Definicion de las escenas del juego.
+    scene("lose", ()=>{
+        add([
+            text("Game Over")
+        ]);
+    })
+    /**Creacion del objeto mario. 
+     * sprite() : Se define un sprite por defecto.
+     * pos()    : Determina la posicion del objeto.
+     * scale()  :
+     * solid()  : Haz que otros objetos no puedan pasar. 
+     *              Requiere compensación de "área".
+     * 
+     * area()   : Genera un área colisionadora a partir 
+     *              de la forma y permite la detección 
+     *              de colisiones.
+     * 
+     * body()   : Cuerpo físico que responde a la gravedad. 
+     *              Requiere los componentes de "área()" y "pos()". 
+     *              Esto también hace que el objeto sea "sólido".
+    */
+    const mario = add([
+        sprite('marioDer'), 
+        pos(90,0),
+        scale(1),
+        solid(),
+        area(),
+        body()
+    ])
+
+    /** Creacion del enemigo hongo.
+     * sprite_frame: posicion inicial del vector de animaciones para el hongo.
+     * vel: velocida del objeto.
+     */
+    const hongo =add ([
+        sprite('enemigo'),
+        pos(460,124),
+        scale(1),
+        area(),
+        //body(),
+        solid(),
+        "hongo",
+        {
+            sprite_frame: 0,
+            vel : ENEMIE_SPEED
+        }
+    ]) 
+
+
+    /**la funcion crea una tortuga cada que es llamada,
+     * la manera de interactuar con esta tortugar es por tags.
+     */
+    function spawnTortuga(){
+        add ([
+            sprite("tortuga1"),
+            pos(460,0),
+            scale(1),
+            area(),
+            body(),
+            solid(),
+            "move",
+            "tortuga",
+            {
+                isJump:false,
+                sprite_frame:0,
+                vel:ENEMIE_SPEED*-1
+            }
+        ])
+    }
+    spawnTortuga();
+
+    /**Registre un evento que se ejecute cada cuadro 
+     * (~60 veces por segundo) para todos los objetos 
+     * del juego con cierta etiqueta.
+     * 
+     * El evento que se esta rastreando es el tag:hongo
+     * 
+     * cada que halla una actualizacion el objeto se movera.
+     * 
+     * cuando llegue al limite del mapa cambiara de direccion.
+     * */
+    onUpdate("hongo", (e) =>{
+        e.move(e.vel,0);
+        if(e.pos.x < 0){
+            e.vel=ENEMIE_SPEED;
+        }else if (e.pos.x>460){
+            e.vel=ENEMIE_SPEED*-1;
+        }
+
+    })
+
+
+    /**Registre un evento que se ejecute cada cuadro 
+     * (~60 veces por segundo) para todos los objetos 
+     * del juego con cierta etiqueta.
+     * 
+     * El evento que se esta rastreando es el tag:tortuga
+     * 
+     * cada que halla una actualizacion el objeto se movera.
+     * 
+     * cuando la tortuga caiga, se destruira.
+     * 
+     * Se creara otra tortuga desde el inicio.
+     * */
+    onUpdate("tortuga", (t)=>{
+        t.move(t.vel, 0);
+        if(t.pos.y>180){
+            destroy(t);
+            spawnTortuga();
+        }else{
+
+        }
+    });
+
+
+
+    /**
+     * Cada 0.2 segundos se cambiaran los sprites de los objetos.
+     * 
+     * 
+     * use()                : permite el cambio de sprite de un objeto.
+     * sprite_frame         : es el indice al que se hara referencia.
+     * enemie_animations    : es el vector de animaciones del hongo,
+     *                          se cambia la animacion dependiendo
+     *                          el sprite_frame
+     * tortura_animations   : es el vector de animaciones de la tortuga.
+     * 
+     */
+    loop(0.2,()=>{
+        hongo.sprite_frame++;
+        if(hongo.sprite_frame>=enemie_animations.length){
+            hongo.sprite_frame=0;
+        }
+        hongo.use(sprite(enemie_animations[hongo.sprite_frame]))
+
+        every("move", (m)=>{
+            m.sprite_frame++;
+            if(m.sprite_frame>=tortuga_animations.length){
+                m.sprite_frame=0;
+            }
+            m.use(sprite(tortuga_animations[m.sprite_frame]))  
+        })
+    })
+
+    
+    /**
+     * para un tiempo aleatorio entre 3 y 5 segundos cada objeto que tenga
+     * el tag: move, ejecutara un salto de 400.
+     * 
+     * isGrounded   : se asegura que este sobre un objeto solido.
+     */
+    loop(rand(3,5), ()=>{
+        every("move", (m)=>{
+            if(m.isGrounded())
+            m.jump(400);
+        })
+        
+    })
+
+
+    onKeyDown("right", ()=>{
+        mario.use(sprite("marioDer")) //cambiar el sprite.
+        mario.move(150,0)
+    })
+
+    onKeyDown("left", ()=>{
+        mario.use(sprite("marioIzq"))  // Cambiar el sprite.
+        mario.move(-150,0)
+    })
+
+    onKeyPress("space",()=>{
+        if(mario.isGrounded())
+        {
+            mario.jump(400)
+            mario.isJump=true;
+        }
+    })
+
+    mario.onUpdate(()=>{
+        if(mario.isGrounded()){
+            mario.isJump=false;
+        }
+        console.log(mario.isJump)
+        camPos(mario.pos)
+    })
+
+    mario.onCollide("hongo", (e)=>{
+        if (mario.isJump){
+            shake();
+            destroy(e)
+        }else{
+            shake();
+            go("lose");
+        }
+        
+    })
+
+})
+go("juego")
